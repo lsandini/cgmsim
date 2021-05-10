71452da4688b68c13d318b3edc1feda78d273eee
Lorenzo Sandini <lsandini@gmail.com>
2021-05-10 21:13:12 +0300

    Add files via upload

--- upload-cgmsim.sh
+++ upload-cgmsim.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-curl -H "Accept: application/json" -H "Content-Type: application/json" https://dmpkl2.herokuapp.com/api/v1/entries/sgv/ | jq '.' > ./files/sgv1.json;
 
 node sgv_start.js;
 
