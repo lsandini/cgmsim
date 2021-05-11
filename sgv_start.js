9e951b73350aa204f5895ca041034afeff625b94
Lorenzo Sandini <lsandini@gmail.com>
2021-05-11 20:06:29 +0300

    Add files via upload

--- sgv_start.js
+++ sgv_start.js
@@ -4,17 +4,6 @@ var moment = require('moment');
 var fs = require('fs');
 const fetch = require('node-fetch');
 
-const api_sgv1 = process.env.API_SGV1;
-
-fetch(api_sgv1)
-    .then(resSGV => resSGV.json())
-    .then(json => {
-        var jsonSGV = JSON.stringify(json, null, 4);
-        fs.writeFile('./files/sgv1.json', jsonSGV, 'utf8', (err) => {
-            if (err) throw err
-            console.log('File created!')
-        })
-    });
 
 const sgvs = require('./files/sgv1.json');
 
