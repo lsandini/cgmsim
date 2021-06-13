4f6741478128e17719ec9880ce56d3871d042046
Lorenzo Sandini <lsandini@gmail.com>
2021-06-13 17:23:10 +0300

    it works !

--- bash2.sh
+++ bash2.sh
@@ -2,64 +2,75 @@
 
 touch test.txt
 rm test.txt
-echo
-echo "First let's gather details about the simulated subject."
-echo
-read -p "What is the weight in kg ? " -r
+
+echo -e "\e[96mFirst let's gather details about the simulated subject.\e[0m "
+echo 
+read  -p "What is the weight in kg ? " -r
+
 WEIGHT=$REPLY
 echo
-
-echo "The absorption or carbs will be AT MOST 360 minutes (or 6 hours). You can specify a shorter duration here."
+echo -e "\e[92mOk, the weight is \e[0m\e[1m\e[92m$WEIGHT kg\e[0m."
 echo
-read -p "What is the longest absorption time for slowly absorbing meals (in minutes) ? " -r
+
+echo -e "\e[96mThe absorption or carbs will be AT MOST 360 minutes (or 6 hours). You can specify a shorter duration here.\e[0m "
+echo
+read -p "What is the longest absorption time for slowly absorbing meals (in minutes) ?  " -r
 CARBS_ABS_TIME=$REPLY
 echo
-
-echo "The profile of the mealtime insulin must be specified next."
+echo -e "\e[92mOk, the maximal carb absorption time will be \e[0m\e[1m\e[92m$CARBS_ABS_TIME minutes.\e[0m "
 echo
-read -p "What is the peak action time (e.g. 55 min for Fiasp, 75 min for Novorapid) ? " -r
+
+echo -e "\e[96mThe profile of the mealtime insulin must be specified next.\e[0m"
+echo
+read -p "What is the peak action time (e.g. 55 min for Fiasp, 75 min for Novorapid) ?  " -r
 TP=$REPLY
 echo
+echo -e "\e[92mOk, the the insulin peak activity will be reached at \e[0m\e[1m\e[92m$TP min.\e[0m"
 
-read -p "What is the total duration of insulin action or DIA (e.g. 5 hours for Fiasp or Novorapid) ? " -r
+echo
+read -p "What is the total duration of insulin action or DIA (e.g. 5 hours for Fiasp or Novorapid) ?  " -r
 DIA=$REPLY
 echo
-
-echo "The simulator needs a few details about the simulated subject."
+echo -e "\e[92mOk, the DIA is set to \e[0m\e[1m\e[92m$DIA hours or 300 min.\e[0m"
 echo
-read -p "What is your typical ISF ? Give only one value for the whole 24h duration:   " -r
+
+echo -e "\e[96mThe simulator needs a more details about the simulated subject.\e[0m"
+echo
+read -p "What is your typical ISF ? Only one value for the whole 24h duration. If necessary, use a dot (.) and not a comma (,) as a decimal separator): " -r
   if [[ $REPLY =~ [0-9] ]]; then
     ISF="$REPLY"
-    echo "Ok, $ISF units will be set as your ISF."
+    echo -e "\e[92mOk, your ISF is set to \e[0m\e[1m\e[92m$ISF mmol/l/h. \e[0m"
   else
     ISF=2
-    echo "Ok, your ISF will be set to 2 for now."
+    echo -e "\e[92mOk, your ISF will be set to \e[0m\e[1m\e[92m2 mmol/l/h \e[0m\e[92mfor now.\e[0m"
   fi
-echo
 
 echo 
-read -p "What is your typical CR (carb ratio, in g/U). If necessary, use a dot (.) and not a comma (,) as a decimal separator:   " -r
+read -p "What is your typical CR (carb ratio, in g/U)? :   " -r
 echo
-
 
   if [[ $REPLY =~ [0-40] ]]; then
     CR="$REPLY"
-    echo "Ok, $CR units will be set as your ISF."
-    echo
+    echo -e "\e[92mOk, \e[0m\e[1m\e[92m$CR units will be set as your carb ratio (CR).\e[0m"
   else
     CR=10
-    echo "Ok, your ISF will be set to 10 for now."
+    echo -e "\e[92mOk, your carb ratio (CR) will be set to \e[0m\e[1m\e[92m10 for now.\e[0m"
   fi
 echo
 
-echo "Now let's set up the connection to your Nightscout website."
-read -p "What is your the MYNIGHTSCOUT part of your Nightscout site URL? (i.e. https://MYNIGHTSCOUT.herokuapp.com)? " -r
+echo -e "\e[96mNow let's set up the connection to your Nightscout website.\e[0m"
+echo
+read -p "What is your the MYNIGHTSCOUT part of your Nightscout site URL? (i.e. https://MYNIGHTSCOUT.herokuapp.com)?  " -r
 NIGHTSCOUT_HOST=$REPLY
 echo
+echo -e "\e[92mYour URL is \e[0m\e[1m\e[92mhttps://$NIGHTSCOUT_HOST.herokuapp.com\e[0m"
+echo
 
-echo "Now let's set up your NS API_SECRET."
-read -p "What is your Nightscout API_SECRET (i.e. myplaintextsecret; It should be at least 12 characters long)? " -r
+echo -e "\e[96mNow let's set up your NS API_SECRET.\e[0m"
+echo
+read -p "What is your Nightscout API_SECRET (i.e. myplaintextsecret; It should be at least 12 characters long)?   " -r
 APISECRET=$REPLY
+echo -e "\e[92m \e[0m\e[1m\e[92m$APISECRET \e[0m\e[92m it is !\e[0m"
 echo
 
 echo ISF=$ISF >> test.txt
@@ -85,6 +96,6 @@ echo API_SGV2="'https://$NIGHTSCOUT_HOST.herokuapp.com/api/v1/entries/'" >> test
 echo >> test.txt
 echo API_SGV1="'https://$NIGHTSCOUT_HOST.herokuapp.com/api/v1/entries/sgv.json'" >> test.txt
 
-
+${ENDCOLOR}
 chmod +x ./bash2.sh
 exit
