5aaa0b7f4367a7b7a52713e7fa5ce5bdf6e5dfc6
Lorenzo Sandini <lsandini@gmail.com>
2021-06-13 14:10:41 +0300

    the API key is now encrypted here

--- upload-cgmsim.js
+++ upload-cgmsim.js
@@ -1,18 +1,28 @@
 const fetch = require('node-fetch');
-const dotenv = require('dotenv');
 var result = require('dotenv').config();
+const crypto = require('crypto');
 
+
+//let's build the API_secret for the headers and the API_url for the fetch() function
+//===================================================================================
 const api_url = process.env.API_SGV2;
-const api_key = process.env.API_KEY;
 
+const hash = crypto.createHash('sha1');
+hash.update(process.env.APISECRET);
+const hashed_secret = hash.digest('hex');
+
+// now the fetch function itself
+//==============================
 const headers = {
     'Content-Type': 'application/json',
-    'api-secret': api_key
+    'api-secret': hashed_secret
 };
 
 const body = require('./files/cgmsim-sgv.json');
 const body_json = JSON.stringify(body);
 console.log(body_json);
+
+
 
 fetch(api_url, {
     method: 'POST',
