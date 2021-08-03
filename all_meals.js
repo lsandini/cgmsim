4c6e37d4dadfc75b40e76525c529f7e9fb374a2f
Lorenzo Sandini <lsandini@gmail.com>
2021-08-03 08:38:25 +0300

    Update all_meals.js
    Added a separate count of 24h carbs for the surprise meal part

--- all_meals.js
+++ all_meals.js
@@ -19,8 +19,9 @@ meals = meal.map(entry => ({ ...entry, time:moment(entry.time).valueOf()}));
 timeSinceMealMin = meals.map(entry => ({ ...entry, mills: entry.time, time: (Date.now() - moment(entry.time).valueOf())/(1000*60)}));
 console.log('this is the trimmed down meals and time since last meal:',timeSinceMealMin);
 
+// this is for the calculations of carbs ingestion
 let lastMeals = timeSinceMealMin.filter(function (e) {
-    return e.time <= 1440; // keep only the meals from the last 24 hours or 1440 min
+    return e.time <= 3600; // keep only the meals from the last 6 hours or 360 min
 });
 console.log('these are the last meals: ',lastMeals);
 
@@ -32,3 +33,19 @@ fs.writeFile('./files/last_meals.json', dataMeals, (err) => {
     }
     console.log("JSON meals data is saved.");
 });
+
+// this is added only for the generation of surprise meals, counted on the amount of carbs in the previous 24h
+let lastMeals24 = timeSinceMealMin.filter(function (e) {
+    return e.time <= 1440; // keep only the meals from the last 24 hours or 1440 min
+});
+console.log('these are the last meals: ',lastMeals24);
+
+const dataMeals24 = JSON.stringify(lastMeals24, null, 4);
+
+fs.writeFile('./files/last_meals24.json', dataMeals, (err) => {
+    if (err) {
+        throw err;
+    }
+    console.log("JSON meals 24h data is saved.");
+});
+
