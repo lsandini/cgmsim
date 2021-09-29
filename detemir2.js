7061a30831ecbe2bc61855bffacca4e9302106d2
Lorenzo Sandini <lsandini@gmail.com>
2021-09-29 17:06:23 +0300

    Update detemir2.js

--- detemir2.js
+++ detemir2.js
@@ -7,8 +7,8 @@ const weight = parseInt(process.env.WEIGHT);
 const detemirs = require('./files/last_detemir.json');
 var jsondet = JSON.stringify(detemirs);
 var detemir_data = JSON.parseWithDate(jsondet);
+var moment = require('moment');
 console.log(detemir_data);
-var moment = require('moment'); 
 
 // activities be expressed as U/min !!!
 let timeSinceDetemirAct = detemir_data.map(entry => {
@@ -18,17 +18,22 @@ let timeSinceDetemirAct = detemir_data.map(entry => {
     var peak = (duration/3);
     var tp = peak;
     var td = duration;
-    var t = (Date.now() - moment(entry.time).valueOf())/(1000*60*60);
+
     var tau = tp * (1 - tp / td) / (1 - 2 * tp / td);
     var a = 2 * tau / td;
     var S = 1 / (1 - a + (1 + a) * Math.exp(-td / tau));
 
     var detemirActivity = 0;
 
-    return { ...entry, time: time, detemirActivity:  dose * (S / Math.pow(tau, 2)) * t * (1 - t / td) * Math.exp(-t / tau)};
+    return { ...entry,
+         time: time,
+         dose:dose,
+         detemirActivity:  (dose * (S / Math.pow(tau, 2)) * time * (1 - time / td) * Math.exp(-time / tau) / 60),
+         tp:tp,
+         td:td
+                };
 });
 console.log('these are the detemir activities:',timeSinceDetemirAct);
-
 // compute the aggregated activity of last detemirs in 30 hours
 
 let lastDetemirs = timeSinceDetemirAct.filter(function (e) {
@@ -36,9 +41,10 @@ let lastDetemirs = timeSinceDetemirAct.filter(function (e) {
 });
 console.log('these are the last detemirs and activities:',lastDetemirs);
 
-var resultDetAct = lastDetemirs.reduce(function(tot, arr) { 
+var resultDetAct = lastDetemirs.reduce(function(tot, arr) {
     return tot + arr.detemirActivity;
   },0);
+
 
 console.log(resultDetAct);
 
